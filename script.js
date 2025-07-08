4bf4934bf63e65c8c644f734417330658ab98e15
Nguyễn Lê Huỳnh <2200000319@nttu.edu.vn>
2025-07-08 11:11:45 +0700

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-// Khởi tạo Swiper slider
 const swiper = new Swiper('.card-wrapper', {
-    loop: true,
-    spaceBetween: 30,
-
-    // Pagination bullets
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true
+  loop: true,
+  spaceBetween: 30,
+  
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-slide-button.swiper-button-next',
-        prevEl: '.swiper-slide-button.swiper-button-prev',
+    768: {
+      slidesPerView: 2,
     },
-
-    // Responsive breakpoints
-    breakpoints: {
-        0: {
-            slidesPerView: 1
-        },
-        768: {
-            slidesPerView: 2
-        },
-        1024: {
-            slidesPerView: 3
-        }
+    1024: {
+      slidesPerView: 3,
     }
-});
-
-// Thêm sự kiện cho nút Message
-document.querySelector('.buttons .btn:first-child').addEventListener('click', () => {
-  alert('Gửi tin nhắn đến Nguyễn Lê Huỳnh');
-});
-
-// Thêm sự kiện cho nút Follow Me
-document.querySelector('.buttons .btn:last-child').addEventListener('click', () => {
-  alert('Bạn đã theo dõi Nguyễn Lê Huỳnh');
+  }
 });
